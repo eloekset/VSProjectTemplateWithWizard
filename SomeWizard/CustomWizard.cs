2ed5c47529e40effc097f28001b361725823d2a4
eloekset <eloekset@msn.com>
2016-08-05 15:44:30 +0200

    VS project template with custom wizard that doesn't work.

---
+++ SomeWizard/CustomWizard.cs
@@ -0,0 +1,35 @@
+﻿using EnvDTE;
+using Microsoft.VisualStudio.TemplateWizard;
+using System.Collections.Generic;
+
+namespace SomeWizard
+{
+    public class CustomWizard : IWizard
+    {
+        public void BeforeOpeningFile(ProjectItem projectItem)
+        {
+        }
+
+        public void ProjectFinishedGenerating(Project project)
+        {
+            project.DTE.ItemOperations.Navigate("http://www.microsoft.com");
+        }
+
+        public void ProjectItemFinishedGenerating(ProjectItem projectItem)
+        {
+        }
+
+        public void RunFinished()
+        {
+        }
+
+        public void RunStarted(object automationObject, Dictionary<string, string> replacementsDictionary, WizardRunKind runKind, object[] customParams)
+        {
+        }
+
+        public bool ShouldAddProjectItem(string filePath)
+        {
+            return true;
+        }
+    }
+}
